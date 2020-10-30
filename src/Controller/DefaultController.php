d37bf417b80da86f453b6e4efe997653287f557c
HongLENOIR <59973797+HongLENOIR@users.noreply.github.com>
2020-10-30 10:50:12 +0100

    DefaultController

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,6 +4,8 @@
 namespace App\Controller;
 
 
+use App\Entity\Category;
+use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class DefaultController extends AbstractController
@@ -14,7 +16,26 @@ class DefaultController extends AbstractController
      */
     public function index()
     {
+        $products =$this->getDoctrine()
+            ->getRepository(Product::class)
+            ->findBy([],['id'=>'DESC'], 8);
         return $this->render('default/index.html.twig');
     }
 
+    public function category($alias)
+    {
+        $category =$this->getDoctrine()
+            ->getRepository(Category::class)
+            ->findOneBy(['alias'=>$alias]);
+        $products=$category->getProducts();
+        return $this->render('default/category.html.twig', ['products'=>$products]);
+    }
+
+    public function product($id)
+    {
+        $product=$this->getDoctrine()
+            ->getRepository(Product::class)
+            ->find($id);
+        return $this->render('default/product.html.twig', ['product'=>$product]);
+    }
 }
